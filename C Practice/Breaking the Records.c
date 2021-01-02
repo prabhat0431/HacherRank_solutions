3d1b3f6be318480bc7b7ef4a863592598361d1ac
Prabhat Chaudhary <75518713+prabhat0431@users.noreply.github.com>
2021-01-02 18:46:51 +0530

    Create Breaking the Records.c

---
+++ C Practice/Breaking the Records.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    int score[n];
+    for(int score_i = 0; score_i < n; score_i++){
+       scanf("%d",&score[score_i]);
+    }
+    int maxScoreAns = 0, minScoreAns = 0, maxScore = score[0], minScore = score[0];
+    for(int i = 1; i < n; i++){
+       if(score[i] > maxScore){
+            maxScore = score[i];
+            maxScoreAns++;
+       }
+       if(score[i] < minScore){
+        minScore = score[i];
+        minScoreAns++;
+       }
+    }
+    printf("%d %d\n", maxScoreAns, minScoreAns);
+    return 0;
+}

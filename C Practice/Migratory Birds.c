62493e2d4abf2425235bf62ca5d60d7c3805aa92
Prabhat Chaudhary <75518713+prabhat0431@users.noreply.github.com>
2021-01-05 20:53:03 +0530

    Create Migratory Birds.c

---
+++ C Practice/Migratory Birds.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+   int i,n,t,a[5],max=0,maxi;
+    a[0]=0;
+    a[1]=0;
+    a[2]=0;
+    a[3]=0;
+    a[4]=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&t);
+        a[t-1]+=1;
+    }
+    for(i=0;i<5;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+            maxi=i+1;
+        }
+    }
+    printf("%d",maxi);
+    return 0;
+}

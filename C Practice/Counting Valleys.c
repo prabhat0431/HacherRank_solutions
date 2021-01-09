e9c70f9116e2ba8c4c420e5a8b17d6c97201f330
Prabhat Chaudhary <75518713+prabhat0431@users.noreply.github.com>
2021-01-09 09:55:14 +0530

    Create Counting Valleys.c

---
+++ C Practice/Counting Valleys.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int num,i,valley=0;
+    scanf("%d",&num);
+    char steps[num];
+    scanf("%s",steps);
+    int sea=0;
+    for(i=0;i<num;i++)
+        {
+        if(sea==0 && steps[i]=='D')
+            valley++;
+        if(steps[i]=='U')
+            sea++;
+        else
+            sea--;
+    }
+    printf("%d",valley);
+    return 0;
+}

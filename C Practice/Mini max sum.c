5fb7515d170a8c56e7be414d7a40b9e2cd28c4ee
Prabhat Chaudhary <75518713+prabhat0431@users.noreply.github.com>
2021-01-26 11:09:47 +0530

    Update Mini max sum.c

--- C Practice/Mini max sum.c
+++ C Practice/Mini max sum.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
-
-int main() {
-     long long a[5];
-    scanf("%lld %lld %lld %lld %lld", &a[0], &a[1], &a[2], &a[3], &a[4]);
-    
-    long long tot = 0;
-    long  max = 0;
-    long  min = 1000000000;
-    int i;
-    for (i = 0; i < 5; ++i) {
-        tot += a[i];
-        if (a[i] > max) max = a[i];
-        if (a[i] < min) min = a[i];
+#include<stdio.h>
+int main()
+{
+    long long max=0,tot=0,a[5],tot_min,tot_max;
+    scanf("%lld %lld %lld %lld %lld",&a[0],&a[1],&a[2],&a[3],&a[4]);
+    long long min=a[0];
+    for(int i=0;i<5;i++)
+    {
+        tot=tot+a[i];
+        if(max<a[i])
+        max=a[i];
+        if(min>a[i])
+        min=a[i];
     }
-    printf("%lld %lld", tot-max, tot-min);
+    tot_max=tot-min;
+    tot_min=tot-max;
+    printf("%lld %lld",tot_min,tot_max);
+    return 0;
 }

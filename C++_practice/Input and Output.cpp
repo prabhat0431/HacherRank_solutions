dd47058fa5c197483143116575d35c52b4fb758b
Prabhat Chaudhary <75518713+prabhat0431@users.noreply.github.com>
2021-01-31 23:33:57 +0530

    Create Input and Output.cpp

---
+++ C++_practice/Input and Output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,n1,n2,sum=0;
+    cin>>n>>n1>>n2;
+    sum=n+n1+n2;
+    cout<<sum;
+    return 0;
+}

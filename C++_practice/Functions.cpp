38870a334182f5f39519b8553eb7978a8065a489
Prabhat Chaudhary <75518713+prabhat0431@users.noreply.github.com>
2021-02-19 20:02:58 +0530

    Create Functions.cpp

---
+++ C++_practice/Functions.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a,int b,int c,int d)
+{
+    int max=0;
+    if(a>b&&a>c&&a>d)
+    cout>>a;
+    if()
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
